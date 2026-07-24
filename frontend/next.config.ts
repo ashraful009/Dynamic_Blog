23768b9fd9143dfeae06ecc6018c283a4ba9faf0
Md_AsHrAFuL_IsLaM <121592381+ashraful009@users.noreply.github.com>
2026-07-24 18:43:24 +0600

    fix: update CSP to allow vercel live iframe

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -36,7 +36,7 @@ const nextConfig: NextConfig = {
           },
           {
             key: "Content-Security-Policy",
-            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://vercel.live; style-src 'self' 'unsafe-inline' fonts.googleapis.com; font-src 'self' fonts.gstatic.com; img-src 'self' data: https: http:; media-src 'self' https: http:; connect-src 'self' http: https: ws: wss:;"
+            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://vercel.live; style-src 'self' 'unsafe-inline' fonts.googleapis.com; font-src 'self' fonts.gstatic.com; img-src 'self' data: https: http:; media-src 'self' https: http:; connect-src 'self' http: https: ws: wss:; frame-src 'self' https://vercel.live;"
           }
         ],
       },
